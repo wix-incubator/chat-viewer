ceb6c14c9e756da4f59585ac737d1cabff3ae558
Denys Dovhan <denysdovhan@gmail.com>
2025-07-11 19:29:06 +0300

    chore: work on demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,166 @@
-import { useState } from 'react'
+import { useMemo, useRef, useState } from 'react';
+import { faker } from '@faker-js/faker';
+import { ChatViewer, ChatViewerHandle } from '../lib'
+import classes from './App.module.css'
 
-import { ChatViewer } from '../lib'
+const WIX_ICON = 'https://simpleicons.org/icons/wix.svg';
 
-console.log(ChatViewer)
+interface Message {
+  id: string;
+  role: 'user' | 'assistant';
+  content: string;
+  name?: string;
+  userpic: string;
+}
+
+function userMessage(): Message {
+  const sex = faker.person.sexType();
+
+  return {
+    id: faker.string.alphanumeric(10),
+    role: 'user',
+    content: faker.lorem.sentences({ min: 1, max: 10 }),
+    name: faker.person.firstName(sex),
+    userpic: faker.image.personPortrait({ sex }),
+  }
+}
+
+function assistantMessage(): Message {
+  return {
+    id: faker.string.alphanumeric(10),
+    role: 'assistant',
+    content: faker.lorem.sentence(),
+    name: "Wix AI Assistant",
+    userpic: WIX_ICON,
+  }
+}
+
+function randomMessage(): Message {
+  const role = faker.helpers.arrayElement(['user', 'assistant']);
+  return role === 'user' ? userMessage() : assistantMessage();
+}
+
+function generateMessages(n: number): Message[] {
+  return Array.from({ length: n }, () => randomMessage());
+}
+
+function Message(props: Message) {
+  return (
+    <div
+      key={props.id}
+      tabIndex={0}
+      className={[classes.message, classes[props.role]].join(' ')}
+    >
+      <img className={classes.userpic} src={props.userpic} alt={`${props.role} avatar`} />
+      <div className={classes.bubble}>
+        <strong>{props.name}</strong>
+        <p>{props.content}</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const chatRef = useRef<ChatViewerHandle<Message>>(null);
+  const [messages, setMessages] = useState<Message[]>(generateMessages(10));
+
+  const [wide, setWide] = useState(true);
+  const [tall, setTall] = useState(true);
+
+  const styles = useMemo(() => ({
+    width: wide ? '100%' : '60%',
+    height: tall ? '100%' : '60%',
+    backgroundColor: '#fefefe',
+  }), [wide, tall]);
+
+  const prependMessages = (n: number) => setMessages((prev) => [...generateMessages(n), ...prev]);
+  const appendMessages = (n: number) => setMessages((prev) => [...prev, ...generateMessages(n)]);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className={classes.body}>
+      <div className={classes.toolbar}>
+        <div className={classes.group}>
+          <strong>Dimensions</strong>
+          <div className={classes.field}>
+            <p>width</p>
+            <button onClick={() => setWide(!wide)}>
+              {wide ? 'Narrow' : 'Wide'}
+            </button>
+          </div>
+          <div className={classes.field}>
+            <p>height</p>
+            <button onClick={() => setTall(!tall)}>
+              {tall ? 'Short' : 'Tall'}
+            </button>
+          </div>
+        </div>
+
+        <div className={classes.group}>
+          <strong>Messages</strong>
+          <div className={classes.field}>
+            <p>prepend</p>
+            <button onClick={() => prependMessages(1)}>
+              1
+            </button>
+            <button onClick={() => prependMessages(10)}>
+              10
+            </button>
+            <button onClick={() => prependMessages(1000)}>
+              1000
+            </button>
+          </div>
+          <div className={classes.field}>
+            <p>append</p>
+            <button onClick={() => appendMessages(1)}>
+              1
+            </button>
+            <button onClick={() => appendMessages(10)}>
+              10
+            </button>
+            <button onClick={() => appendMessages(1000)}>
+              1000
+            </button>
+          </div>
+          <div className={classes.field}>
+            <p>clear</p>
+            <button onClick={() => setMessages([])}>
+              Clear
+            </button>
+          </div>
+        </div>
+        
+        <div className={classes.group}>
+          <strong>Actions</strong>
+          <div className={classes.field}>
+            <p>jump to</p>
+            <button onClick={() => chatRef.current?.scrollToTop()}>
+              Top
+            </button>
+            <button onClick={() => chatRef.current?.scrollToBottom()}>
+              Bottom
+            </button>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className={classes.content}>
+        <ChatViewer<Message>
+          ref={chatRef}
+          style={styles}
+          messages={messages}
+          renderMessage={(message) => <Message {...message} />}
+        />
+      </div>
+      <div className={classes.debug}>
+        <strong>Items (index: id)</strong>
+        <pre>
+          {messages.map((msg, idx) => (
+            <div key={msg.id}>
+              {idx}: {msg.id}
+            </div>
+          ))}
+        </pre>
+      </div>
+    </div>
   )
 }
 
