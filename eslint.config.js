badd23a3cb070926d8995e4d5b2c5e2ed5178f03
Denys Dovhan <denysdovhan@gmail.com>
2026-07-03 17:28:01 +0300

    chore: use mature typescript-eslint release

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,21 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { globalIgnores } from 'eslint/config';
 
-export default defineConfig([
+export default tseslint.config([
   globalIgnores(['dist', 'node_modules', 'website', '.yarn']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tsPlugin.configs['flat/recommended'],
+      ...tseslint.configs.recommended,
       reactRefresh.configs.vite,
       prettier,
     ],
     languageOptions: {
-      parser: tsParser,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
