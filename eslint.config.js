8527d4171e3bc66412ed2813a0351e6c0131e988
Denys Dovhan <denysdovhan@gmail.com>
2026-07-03 17:23:35 +0300

    chore: avoid npq provenance regressions

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,23 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
-import { globalIgnores } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist', 'node_modules', 'website', '.yarn']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      ...tsPlugin.configs['flat/recommended'],
       reactRefresh.configs.vite,
       prettier,
     ],
-    plugins: {
-      'react-hooks': reactHooks,
-    },
-    rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
